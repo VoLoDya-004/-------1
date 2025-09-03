2dbc58fcce83d153f39c738964414bccee39d356
Silaev Vladimir <vd-silaev@yandex.ru>
2025-09-03 16:38:08 +0300

    1.0 commit

---
+++ layout1/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+
+export default function Header() {
+
+
+    return (
+        <header className="header">
+            <div className="container">
+                <h1>Be aware,  Manage well.</h1>
+                <p>
+                    Everyone experience stress in different ways.
+                    Let Alivio guide you, in a personalized journal experience, to overcome your stress.
+                </p>
+                <button>Find Your Way</button>
+            </div>
+        </header>
+    )
+}
